33ff92b980386ee80a19bd09ac0ab32472b0b065
neinomas <neinomas@gmail.com>
2021-03-24 18:09:12 +0000

    some changes to the ami, we're now using a custom made one

--- instance/main.tf
+++ instance/main.tf
@@ -1,7 +1,9 @@
 resource "aws_instance" "jenkins" {
-  ami           = "ami-096cb92bb3580c759"
-  instance_type = "t2.micro"
+  ami           = "ami-0f387361de5dcb66d"
+  instance_type = "t2.medium"
   key_name      = "jenkins"
+  #user_data = "${file("install_jenkins.sh")}"
+  
   network_interface {
     network_interface_id = var.jenkins_net_id
     device_index         = 0
@@ -14,8 +16,10 @@ resource "aws_instance" "jenkins" {
 
 resource "aws_instance" "bastion" {
   ami           = "ami-096cb92bb3580c759"
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"
   key_name      = "bastion"
+  
+
   network_interface {
     network_interface_id = var.bastion_net_id
     device_index         = 0
