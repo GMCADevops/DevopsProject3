e24dbdeed70c89f29b72788ab11829d4c0bbf3bc
Dale Walker <d.walker0357@gmail.com>
2021-03-25 10:13:34 +0000

    Updated Deployment Script

--- jenkins/deployment.sh
+++ jenkins/deployment.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kubectl -f apply nginx-configmap.yaml -f backend.yaml -f frontend.yaml --validate = false -f loadbalancer.yaml # Deploy all the kubernetes .yaml files to build 
+kubectl apply -f nginx-configmap.yaml -f backend.yaml -f frontend.yaml --validate=false -f loadbalancer.yaml # Deploy all the kubernetes .yaml files to build 
