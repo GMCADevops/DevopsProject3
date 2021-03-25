a41c7da0b4741d071ad036729d86212146102d74
Dale Walker <d.walker0357@gmail.com>
2021-03-25 15:43:43 +0000

    Ansible Changes

--- infrastructure-deployment.sh
+++ infrastructure-deployment.sh
@@ -15,4 +15,5 @@ cd .. # Jump back a directory
 cd ansible # Switch to the ansible directory 
 
 
-ansible-playbook playbook.yaml -i inventory.yaml # Run the ansible playbook to congfiure the deployed infrastructure
+# Run the ansible playbook to configure the deployed infrastructure
+ansible-playbook playbook.yaml –i inventory --extra-vars "bastion_ip=${terraform output bastion_ip} "ci-cd_ip=${terraform output ci-cd_ip} "node-1_ip=${terraform output node-1_ip} "node-2_ip=${terraform output node-2_ip} "node-3_ip=${terraform output node-3_ip}
